35a08650798240996aa621611002d3e161ce9243
Christian Majid <josh.majid@hotmail.com>
2024-11-20 12:30:50 -0500

    fixing xml to include our logo

--- app/src/main/java/com/example/yololitertobjectdetection/Constants.kt
+++ app/src/main/java/com/example/yololitertobjectdetection/Constants.kt
@@ -2,5 +2,5 @@ package com.example.yololitertobjectdetection
 
 object Constants {
     const val MODEL_PATH = "best_float32v10.tflite"
-    val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
+    val LABELS_PATH: String? = null //add labels if we need it
 }
